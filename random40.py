1fd6c408569b89f3c37e24f507954861a44002be
SARPAINe <35373251+SARPAINe@users.noreply.github.com>
2021-03-09 01:01:09 +0600

    added gui using tkinter

--- random40.py
+++ random40.py
@@ -1,3 +1,8 @@
+# importing required module 
+from tkinter import *
+from threading import *
+import time
+from gtts import gTTS 
 import re
 import pyttsx3
 import speech_recognition as sr
@@ -7,24 +12,27 @@ import webbrowser
 import os
 import smtplib
 import pyaudio
-from selenium import webdriver
-from selenium.webdriver.common.keys import Keys
-import sys
+import pywhatkit
+import pyjokes
+from googlesearch import search 
 
+r = sr.Recognizer()
 
 engine = pyttsx3.init('sapi5')
 voices = engine.getProperty('voices')
 # print(voices[1].id)
 engine.setProperty('voice', voices[0].id)
 
+def threading(): 
+    # Call work function 
+    t1=Thread(target=play) 
+    t1.start() 
 
 def speak(audio):
     engine.say(audio)
     engine.runAndWait()
 
-
-
-def wishMe():
+def wishMe(): 
     hour = int(datetime.datetime.now().hour)
     if hour>=0 and hour<12:
         speak("Good Morning! and Welcome back! Mr. Refat")
@@ -35,11 +43,46 @@ def wishMe():
     else:
         speak("Good Evening! and Welcome back! Mr. Refat")  
 
-    speak("Sir, I am RANDOM40. Please tell me how may I help you")       
+    speak("Sir, I am RANDOM40. Please tell me how may I help you") 
+
+
+# this module helps to 
+# play the converted audio 
+import os 
+
+# create tkinter window 
+root = Tk() 
+
+
+# styling the frame which helps to 
+# make our background stylish 
+frame1 = Frame(root, 
+			bg = "lightPink", 
+			height = "150") 
+
+# plcae the widget in gui window 
+frame1.pack(fill = X) 
+
+
+frame2 = Frame(root, 
+			bg = "lightgreen", 
+			height = "750") 
+frame2.pack(fill=X) 
+
+
+
+# styling the label which show the text 
+# in our tkinter window 
+label = Label(frame1, text = "Personal Assistant", 
+			font = "bold, 30", 
+			bg = "lightpink") 
+
+label.place(x = 110, y = 70) 
+
+
 
 def takeCommand():
     #It takes microphone input from the user and returns string output
-
     r = sr.Recognizer()
     with sr.Microphone() as source:
         print("Listening...")
@@ -59,21 +102,10 @@ def takeCommand():
         return "None"
     return query
 
-def sendEmail(to, content):
-    server = smtplib.SMTP('smtp.gmail.com', 587)
-    server.ehlo()
-    server.starttls()
-    server.login('youremail@gmail.com', 'your-password')
-    server.sendmail('youremail@gmail.com', to, content)
-    server.close()
-
-if __name__ == "__main__":
-    wishMe()
+def play(): 
+    # wishMe()
     while True:
-    # if 1:
         query = takeCommand().lower()
-
-        # Logic for executing tasks based on query
         if 'wikipedia' in query:
             speak('Searching Wikipedia...')
             query = query.replace("wikipedia", "")
@@ -81,64 +113,81 @@ if __name__ == "__main__":
             speak("According to Wikipedia")
             print(results)
             speak(results)
+        
+        elif 'joke' in query:
+            speak(pyjokes.get_joke())
 
-        elif 'open google' in query:
-            reg_ex = re.search('open google and search (.*)', query)
-            search_for = query.split("search",1)[1]
-            url = 'https://www.google.com/'
-            if reg_ex:
-                subgoogle = reg_ex.group(1)
-                url = url + 'r/' + subgoogle
-            speak('Okay!')
-            driver = webdriver.Firefox(executable_path='E:\\Software\\geckodriver') #depends which web browser you are using
-            driver.get('http://www.google.com')
-            search = driver.find_element_by_name('q') # finds search
-            search.send_keys(str(search_for)) #sends search keys 
-            search.send_keys(Keys.RETURN) #hits enter
+        elif 'play' in query:
+            song = query.replace('play', '')
+            speak('playing ' + song)
+            pywhatkit.playonyt(song)
+
+        elif 'google search' in query:
+            query = query.replace("google search ", "")
+            query=query.replace(" ","+");
+            url="https://www.google.com/search?q="+query
+            chrome_browser = webbrowser.get("C:/Program Files (x86)/Google/Chrome/Application/chrome.exe %s")
+            chrome_browser.open_new_tab(url)
+            # webbrowser.open_new_tab(url)
         
         elif 'go to sleep' in query:
-             speak('Shutting down...')
-             sys.exit(0)
+            speak('Shutting down...')
+            sys.exit(0)
 
         elif 'open youtube' in query:
             speak('Opening Youtube...')
-            webbrowser.open("youtube.com")
+            chrome_browser = webbrowser.get("C:/Program Files (x86)/Google/Chrome/Application/chrome.exe %s")
+            chrome_browser.open_new_tab("youtube.com")
+            # webbrowser.open_new_tab("youtube.com")
 
-        elif 'open google' in query:
-            webbrowser.open("google.com")
-
-        elif 'open stackoverflow' in query:
-            webbrowser.open("stackoverflow.com")   
-
-
-        elif 'play' in query:
-            music_dir = 'D:\\MUSIC\\audio music\\English'
-            # D:\MUSIC\audio music\English
-            songs = os.listdir(music_dir)
-            print(songs)    
-            os.startfile(os.path.join(music_dir, songs[1]))
+        elif 'open stack overflow' in query:
+            webbrowser.open_new_tab("stackoverflow.com")   
 
         elif 'time' in query:
             strTime = datetime.datetime.now().strftime("%H:%M:%S")    
             speak(f"Sir, the time is {strTime}")
+        
+        elif 'suggest app' in query:
+            speak('your most used apps are Codeblocks, Microsoft word, Spotify. Do you want to open one?')
 
-        elif 'open code' in query:
+        elif 'open code blocks' in query:
             speak('Opening codeblocks sir! Please wait a bit')
             codePath = "C:\\Program Files (x86)\\CodeBlocks\\codeblocks.exe"
-            os.startfile(codePath)
+            os.startfile(codePath) 
+        
+        elif 'open microsoft word' in query:
+            speak('Opening word sir! Please wait a bit')
+            codePath = "C:\\Program Files\\Microsoft Office\\root\\Office16\\WINWORD.EXE"
+            os.startfile(codePath) 
+        
+        elif 'open spotify' in query:
+            speak('Opening spotify sir! Please wait a bit')
+            codePath = "C:\\Users\\Refat\\AppData\\Roaming\\Spotify\\Spotify.exe"
+            os.startfile(codePath) 
+        
+        
+        
 
-        elif 'idm' in query:
-            speak('Opening internet download manager!')
-            codePath = "C:\\Program Files (x86)\\Internet Download Manager\\IDMan.exe"
-            os.startfile(codePath)
 
-        elif 'email to harry' in query:
-            try:
-                speak("What should I say?")
-                content = takeCommand()
-                to = "harryyourEmail@gmail.com"    
-                sendEmail(to, content)
-                speak("Email has been sent!")
-            except Exception as e:
-                print(e)
-                speak("Sorry my friend harry bhai. I am not able to send this email")    
+btn = Button(frame2, text = "COMMAND", 
+			width = "15", pady = 10, 
+			font = "bold, 15", 
+			command = threading, bg='yellow') 
+
+btn.place(x = 200, 
+		y = 130) 
+
+# give a title 
+root.title("Personal Assistant") 
+
+
+
+# we can not change the size 
+# if you want you can change 
+root.geometry("650x550+350+200") 
+
+# start the gui 
+# wishMe()
+
+root.after(1000, wishMe) 
+root.mainloop() 
